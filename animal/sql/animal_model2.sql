94266ee34ecde7f3088f981f71d4effd1e752515
jeongjiho <jeongjiho@jeongjihoui-MacBook-Pro.local>
2016-01-06 17:51:09 +0900

    sql과 exerd 몇개 변경하고 다시 올립니다 data2, model2로 db 등록하세요

---
+++ animal/sql/animal_model2.sql
@@ -0,0 +1,500 @@
+-- 회원
+DROP TABLE IF EXISTS Member RESTRICT;
+
+-- 쪽지
+DROP TABLE IF EXISTS Message RESTRICT;
+
+-- 교육
+DROP TABLE IF EXISTS Education RESTRICT;
+
+-- 분양
+DROP TABLE IF EXISTS Parcel_Out RESTRICT;
+
+-- 짝짓기
+DROP TABLE IF EXISTS Mating RESTRICT;
+
+-- 산책코스
+DROP TABLE IF EXISTS TABLE6 RESTRICT;
+
+-- 댓글
+DROP TABLE IF EXISTS Comment RESTRICT;
+
+-- 팻
+DROP TABLE IF EXISTS Pet RESTRICT;
+
+-- 다이어리 - 일정
+DROP TABLE IF EXISTS Schedule RESTRICT;
+
+-- 파일
+DROP TABLE IF EXISTS File RESTRICT;
+
+-- 예방접종
+DROP TABLE IF EXISTS TABLE12 RESTRICT;
+
+-- 테이블코드
+DROP TABLE IF EXISTS TABLE13 RESTRICT;
+
+-- 카테고리
+DROP TABLE IF EXISTS Category RESTRICT;
+
+-- 회원
+CREATE TABLE Member (
+  MEM_NUM   INTEGER      NOT NULL COMMENT '회원번호', -- 회원번호
+  MEM_NAME  VARCHAR(50)  NOT NULL COMMENT '이름', -- 이름
+  MEM_EMAIL VARCHAR(50)  NOT NULL COMMENT '이메일', -- 이메일
+  MEM_PWD   VARCHAR(255) NOT NULL COMMENT '비밀번호', -- 비밀번호
+  MEM_TEL   VARCHAR(20)  NULL     COMMENT '전화번호', -- 전화번호
+  MEM_IMG   VARCHAR(50)  NULL     COMMENT '회원이미지' -- 회원이미지
+)
+COMMENT '회원';
+
+-- 회원
+ALTER TABLE Member
+  ADD CONSTRAINT PK_Member -- 회원 기본키
+    PRIMARY KEY (
+      MEM_NUM -- 회원번호
+    );
+
+-- 회원 유니크 인덱스
+CREATE UNIQUE INDEX UIX_Member
+  ON Member ( -- 회원
+    MEM_EMAIL ASC -- 이메일
+  );
+
+ALTER TABLE Member
+  MODIFY COLUMN MEM_NUM INTEGER NOT NULL AUTO_INCREMENT COMMENT '회원번호';
+
+-- 쪽지
+CREATE TABLE Message (
+  MSG_NUM     INTEGER      NOT NULL COMMENT '쪽지번호', -- 쪽지번호
+  MEM_NUM     INTEGER      NOT NULL COMMENT '보내는이', -- 보내는이
+  MEM_NUM2    INTEGER      NOT NULL COMMENT '받는이', -- 받는이
+  MSG_CONTENT VARCHAR(255) NOT NULL COMMENT '내용', -- 내용
+  COL         BOOLEAN      NOT NULL DEFAULT false COMMENT '조회상태' -- 조회상태
+)
+COMMENT '쪽지';
+
+-- 쪽지
+ALTER TABLE Message
+  ADD CONSTRAINT PK_Message -- 쪽지 기본키
+    PRIMARY KEY (
+      MSG_NUM -- 쪽지번호
+    );
+
+ALTER TABLE Message
+  MODIFY COLUMN MSG_NUM INTEGER NOT NULL AUTO_INCREMENT COMMENT '쪽지번호';
+
+-- 교육
+CREATE TABLE Education (
+  EDU_NUM     INTEGER      NOT NULL COMMENT '교육번호', -- 교육번호
+  EDU_VIEWS   INTEGER      NULL     COMMENT '조회수', -- 조회수
+  EDU_CRE     DATETIME     NOT NULL COMMENT '작성일', -- 작성일
+  EDU_TITLE   VARCHAR(50)  NOT NULL COMMENT '제목', -- 제목
+  EDU_CONTENT VARCHAR(255) NOT NULL COMMENT '내용', -- 내용
+  EDU_DIV     VARCHAR(50)  NOT NULL COMMENT '구분', -- 구분
+  MEM_NUM     INTEGER      NULL     COMMENT '회원번호' -- 회원번호
+)
+COMMENT '교육';
+
+-- 교육
+ALTER TABLE Education
+  ADD CONSTRAINT PK_Education -- 교육 기본키
+    PRIMARY KEY (
+      EDU_NUM -- 교육번호
+    );
+
+ALTER TABLE Education
+  MODIFY COLUMN EDU_NUM INTEGER NOT NULL AUTO_INCREMENT COMMENT '교육번호';
+
+-- 분양
+CREATE TABLE Parcel_Out (
+  PO_NUM     INTEGER      NOT NULL COMMENT '분양번호', -- 분양번호
+  PO_CREDT   DATETIME     NOT NULL COMMENT '작성일', -- 작성일
+  PO_TITLE   VARCHAR(50)  NOT NULL COMMENT '제목', -- 제목
+  PO_CONTENT VARCHAR(255) NOT NULL COMMENT '내용', -- 내용
+  PO_DIV     VARCHAR(50)  NOT NULL COMMENT '구분', -- 구분
+  PO_SPECIES VARCHAR(50)  NOT NULL COMMENT '종류', -- 종류
+  PO_COST    VARCHAR(50)  NULL     COMMENT '판매금', -- 판매금
+  MEM_NUM    INTEGER      NULL     COMMENT '회원번호' -- 회원번호
+)
+COMMENT '분양';
+
+-- 분양
+ALTER TABLE Parcel_Out
+  ADD CONSTRAINT PK_Parcel_Out -- 분양 기본키
+    PRIMARY KEY (
+      PO_NUM -- 분양번호
+    );
+
+ALTER TABLE Parcel_Out
+  MODIFY COLUMN PO_NUM INTEGER NOT NULL AUTO_INCREMENT COMMENT '분양번호';
+
+-- 짝짓기
+CREATE TABLE Mating (
+  MAT_NUM     INTEGER      NOT NULL COMMENT '짝짓기번호', -- 짝짓기번호
+  MAT_VIEWS   INTEGER      NOT NULL DEFAULT 0 COMMENT '조회수', -- 조회수
+  MAT_CRE     DATETIME     NOT NULL COMMENT '작성일', -- 작성일
+  MAT_TITLE   VARCHAR(50)  NOT NULL COMMENT '제목', -- 제목
+  MAT_CONTENT VARCHAR(255) NOT NULL COMMENT '내용', -- 내용
+  MAT_DIV     VARCHAR(50)  NOT NULL COMMENT '구분', -- 구분
+  MAT_SPECIES VARCHAR(50)  NOT NULL COMMENT '종', -- 종
+  MAT_SEX     CHAR(5)      NOT NULL COMMENT '성별', -- 성별
+  MAT_COST    VARCHAR(50)  NULL     COMMENT '중매금', -- 중매금
+  MEM_NUM     INTEGER      NULL     COMMENT '회원번호' -- 회원번호
+)
+COMMENT '짝짓기';
+
+-- 짝짓기
+ALTER TABLE Mating
+  ADD CONSTRAINT PK_Mating -- 짝짓기 기본키
+    PRIMARY KEY (
+      MAT_NUM -- 짝짓기번호
+    );
+
+ALTER TABLE Mating
+  MODIFY COLUMN MAT_NUM INTEGER NOT NULL AUTO_INCREMENT COMMENT '짝짓기번호';
+
+-- 산책코스
+CREATE TABLE TABLE6 (
+  COL     <데이터 타입 없음> NOT NULL COMMENT '산책번호', -- 산책번호
+  COL2    <데이터 타입 없음> NULL     COMMENT '조회수', -- 조회수
+  COL6    <데이터 타입 없음> NULL     COMMENT '좋아요', -- 좋아요
+  SCH_NUM INTEGER            NULL     COMMENT '일정번호', -- 일정번호
+  COL6    <데이터 타입 없음> NULL     COMMENT '카테고리번호', -- 카테고리번호
+  COL3    <데이터 타입 없음> NULL     COMMENT '카테고리번호2' -- 카테고리번호2
+)
+COMMENT '산책코스';
+
+-- 산책코스
+ALTER TABLE TABLE6
+  ADD CONSTRAINT PK_TABLE6 -- 산책코스 기본키
+    PRIMARY KEY (
+      COL -- 산책번호
+    );
+
+-- 댓글
+CREATE TABLE Comment (
+  CMT_NUM     INTEGER      NOT NULL COMMENT '댓글번호', -- 댓글번호
+  CMT_CONTENT VARCHAR(255) NOT NULL COMMENT '내용', -- 내용
+  MEM_NUM     INTEGER      NULL     COMMENT '회원번호', -- 회원번호
+  SCH_NUM     INTEGER      NULL     COMMENT '일정번호', -- 일정번호
+  EDU_NUM     INTEGER      NULL     COMMENT '교육번호', -- 교육번호
+  PO_NUM      INTEGER      NULL     COMMENT '분양번호', -- 분양번호
+  MAT_NUM     INTEGER      NULL     COMMENT '짝짓기번호' -- 짝짓기번호
+)
+COMMENT '댓글';
+
+-- 댓글
+ALTER TABLE Comment
+  ADD CONSTRAINT PK_Comment -- 댓글 기본키
+    PRIMARY KEY (
+      CMT_NUM -- 댓글번호
+    );
+
+ALTER TABLE Comment
+  MODIFY COLUMN CMT_NUM INTEGER NOT NULL AUTO_INCREMENT COMMENT '댓글번호';
+
+-- 팻
+CREATE TABLE Pet (
+  PET_NUM     INTEGER      NOT NULL COMMENT '팻번호', -- 팻번호
+  MEM_NUM     INTEGER      NOT NULL COMMENT '회원번호', -- 회원번호
+  PET_NAME    VARCHAR(50)  NOT NULL COMMENT '펫이름', -- 펫이름
+  PET_SPECIES VARCHAR(50)  NOT NULL COMMENT '종', -- 종
+  PET_IMG     VARCHAR(255) NULL     COMMENT '이미지', -- 이미지
+  TAG_COLOR   VARCHAR(50)  NOT NULL COMMENT '태그색', -- 태그색
+  INJECTION   VARCHAR(50)  NULL     COMMENT '주사', -- 주사
+  MEDICINE    VARCHAR(50)  NULL     COMMENT '약 ' -- 약 
+)
+COMMENT '팻';
+
+-- 팻
+ALTER TABLE Pet
+  ADD CONSTRAINT PK_Pet -- 팻 기본키
+    PRIMARY KEY (
+      PET_NUM -- 팻번호
+    );
+
+-- 팻 유니크 인덱스
+CREATE UNIQUE INDEX UIX_Pet
+  ON Pet ( -- 팻
+  );
+
+ALTER TABLE Pet
+  MODIFY COLUMN PET_NUM INTEGER NOT NULL AUTO_INCREMENT COMMENT '팻번호';
+
+-- 다이어리 - 일정
+CREATE TABLE Schedule (
+  SCH_NUM     INTEGER      NOT NULL COMMENT '일정번호', -- 일정번호
+  PET_NUM     INTEGER      NOT NULL COMMENT '팻번호', -- 팻번호
+  START_DATE  DATETIME     NOT NULL COMMENT '시작일', -- 시작일
+  END_DATE    DATETIME     NOT NULL COMMENT '종료일', -- 종료일
+  SCH_CRE     DATETIME     NOT NULL COMMENT '작성일', -- 작성일
+  SCH_CONTENT VARCHAR(255) NULL     COMMENT '내용', -- 내용
+  SCH_TITLE   VARCHAR(50)  NULL     COMMENT '제목', -- 제목
+  CATE_CODE   CHAR(6)      NULL     COMMENT '카테고리코드', -- 카테고리코드
+  NOTI_OPEN   INTEGER      NOT NULL COMMENT '공개여부', -- 공개여부
+  NOTI_VIEWS  INTEGER      NOT NULL DEFAULT 0 COMMENT '조회수', -- 조회수
+  NOTI_LIKES  INTEGER      NULL     COMMENT '좋아요' -- 좋아요
+)
+COMMENT '다이어리 - 일정';
+
+-- 다이어리 - 일정
+ALTER TABLE Schedule
+  ADD CONSTRAINT PK_Schedule -- 다이어리 - 일정 기본키
+    PRIMARY KEY (
+      SCH_NUM -- 일정번호
+    );
+
+ALTER TABLE Schedule
+  MODIFY COLUMN SCH_NUM INTEGER NOT NULL AUTO_INCREMENT COMMENT '일정번호';
+
+-- 파일
+CREATE TABLE File (
+  FILE_NUM  INTEGER     NOT NULL COMMENT '파일번호', -- 파일번호
+  FILE_NAME VARCHAR(50) NOT NULL COMMENT '파일명', -- 파일명
+  NOTI_NUM  INTEGER     NULL     COMMENT '게시물번호', -- 게시물번호
+  EDU_NUM   INTEGER     NULL     COMMENT '교육번호', -- 교육번호
+  MAT_NUM   INTEGER     NULL     COMMENT '짝짓기번호', -- 짝짓기번호
+  PO_NUM    INTEGER     NULL     COMMENT '분양번호', -- 분양번호
+  SCH_NUM   INTEGER     NULL     COMMENT '일정번호' -- 일정번호
+)
+COMMENT '파일';
+
+-- 파일
+ALTER TABLE File
+  ADD CONSTRAINT PK_File -- 파일 기본키
+    PRIMARY KEY (
+      FILE_NUM -- 파일번호
+    );
+
+ALTER TABLE File
+  MODIFY COLUMN FILE_NUM INTEGER NOT NULL AUTO_INCREMENT COMMENT '파일번호';
+
+-- 예방접종
+CREATE TABLE TABLE12 (
+  COL  <데이터 타입 없음> NOT NULL COMMENT '예방접종번호', -- 예방접종번호
+  COL2 <데이터 타입 없음> NULL     COMMENT 'D_day', -- D_day
+  COL3 <데이터 타입 없음> NULL     COMMENT '새 컬럼' -- 새 컬럼
+)
+COMMENT '예방접종';
+
+-- 예방접종
+ALTER TABLE TABLE12
+  ADD CONSTRAINT PK_TABLE12 -- 예방접종 기본키
+    PRIMARY KEY (
+      COL -- 예방접종번호
+    );
+
+-- 테이블코드
+CREATE TABLE TABLE13 (
+  COL  <데이터 타입 없음> NOT NULL COMMENT '테이블코드', -- 테이블코드
+  COL2 <데이터 타입 없음> NULL     COMMENT '테이블명' -- 테이블명
+)
+COMMENT '테이블코드';
+
+-- 테이블코드
+ALTER TABLE TABLE13
+  ADD CONSTRAINT PK_TABLE13 -- 테이블코드 기본키
+    PRIMARY KEY (
+      COL -- 테이블코드
+    );
+
+-- 카테고리
+CREATE TABLE Category (
+  CATE_CODE CHAR(6)     NOT NULL COMMENT '카테고리코드', -- 카테고리코드
+  CATE_NAME VARCHAR(50) NOT NULL COMMENT '카테고리명' -- 카테고리명
+)
+COMMENT '카테고리';
+
+-- 카테고리
+ALTER TABLE Category
+  ADD CONSTRAINT PK_Category -- 카테고리 기본키
+    PRIMARY KEY (
+      CATE_CODE -- 카테고리코드
+    );
+
+-- 카테고리 유니크 인덱스
+CREATE UNIQUE INDEX UIX_Category
+  ON Category ( -- 카테고리
+    CATE_NAME ASC -- 카테고리명
+  );
+
+-- 쪽지
+ALTER TABLE Message
+  ADD CONSTRAINT FK_Member_TO_Message -- 회원 -> 쪽지
+    FOREIGN KEY (
+      MEM_NUM -- 보내는이
+    )
+    REFERENCES Member ( -- 회원
+      MEM_NUM -- 회원번호
+    );
+
+-- 쪽지
+ALTER TABLE Message
+  ADD CONSTRAINT FK_Member_TO_Message2 -- 회원 -> 쪽지2
+    FOREIGN KEY (
+      MEM_NUM2 -- 받는이
+    )
+    REFERENCES Member ( -- 회원
+      MEM_NUM -- 회원번호
+    );
+
+-- 교육
+ALTER TABLE Education
+  ADD CONSTRAINT FK_Member_TO_Education -- 회원 -> 교육
+    FOREIGN KEY (
+      MEM_NUM -- 회원번호
+    )
+    REFERENCES Member ( -- 회원
+      MEM_NUM -- 회원번호
+    );
+
+-- 분양
+ALTER TABLE Parcel_Out
+  ADD CONSTRAINT FK_Member_TO_Parcel_Out -- 회원 -> 분양
+    FOREIGN KEY (
+      MEM_NUM -- 회원번호
+    )
+    REFERENCES Member ( -- 회원
+      MEM_NUM -- 회원번호
+    );
+
+-- 짝짓기
+ALTER TABLE Mating
+  ADD CONSTRAINT FK_Member_TO_Mating -- 회원 -> 짝짓기
+    FOREIGN KEY (
+      MEM_NUM -- 회원번호
+    )
+    REFERENCES Member ( -- 회원
+      MEM_NUM -- 회원번호
+    );
+
+-- 산책코스
+ALTER TABLE TABLE6
+  ADD CONSTRAINT FK_Schedule_TO_TABLE6 -- 다이어리 - 일정 -> 산책코스
+    FOREIGN KEY (
+      SCH_NUM -- 일정번호
+    )
+    REFERENCES Schedule ( -- 다이어리 - 일정
+      SCH_NUM -- 일정번호
+    );
+
+-- 댓글
+ALTER TABLE Comment
+  ADD CONSTRAINT FK_Member_TO_Comment -- 회원 -> 댓글
+    FOREIGN KEY (
+      MEM_NUM -- 회원번호
+    )
+    REFERENCES Member ( -- 회원
+      MEM_NUM -- 회원번호
+    );
+
+-- 댓글
+ALTER TABLE Comment
+  ADD CONSTRAINT FK_Education_TO_Comment -- 교육 -> 댓글
+    FOREIGN KEY (
+      EDU_NUM -- 교육번호
+    )
+    REFERENCES Education ( -- 교육
+      EDU_NUM -- 교육번호
+    );
+
+-- 댓글
+ALTER TABLE Comment
+  ADD CONSTRAINT FK_Parcel_Out_TO_Comment -- 분양 -> 댓글
+    FOREIGN KEY (
+      PO_NUM -- 분양번호
+    )
+    REFERENCES Parcel_Out ( -- 분양
+      PO_NUM -- 분양번호
+    );
+
+-- 댓글
+ALTER TABLE Comment
+  ADD CONSTRAINT FK_Mating_TO_Comment -- 짝짓기 -> 댓글
+    FOREIGN KEY (
+      MAT_NUM -- 짝짓기번호
+    )
+    REFERENCES Mating ( -- 짝짓기
+      MAT_NUM -- 짝짓기번호
+    );
+
+-- 댓글
+ALTER TABLE Comment
+  ADD CONSTRAINT FK_Schedule_TO_Comment -- 다이어리 - 일정 -> 댓글
+    FOREIGN KEY (
+      SCH_NUM -- 일정번호
+    )
+    REFERENCES Schedule ( -- 다이어리 - 일정
+      SCH_NUM -- 일정번호
+    );
+
+-- 팻
+ALTER TABLE Pet
+  ADD CONSTRAINT FK_Member_TO_Pet -- 회원 -> 팻
+    FOREIGN KEY (
+      MEM_NUM -- 회원번호
+    )
+    REFERENCES Member ( -- 회원
+      MEM_NUM -- 회원번호
+    );
+
+-- 다이어리 - 일정
+ALTER TABLE Schedule
+  ADD CONSTRAINT FK_Pet_TO_Schedule -- 팻 -> 다이어리 - 일정
+    FOREIGN KEY (
+      PET_NUM -- 팻번호
+    )
+    REFERENCES Pet ( -- 팻
+      PET_NUM -- 팻번호
+    );
+
+-- 다이어리 - 일정
+ALTER TABLE Schedule
+  ADD CONSTRAINT FK_Category_TO_Schedule -- 카테고리 -> 다이어리 - 일정
+    FOREIGN KEY (
+      CATE_CODE -- 카테고리코드
+    )
+    REFERENCES Category ( -- 카테고리
+      CATE_CODE -- 카테고리코드
+    );
+
+-- 파일
+ALTER TABLE File
+  ADD CONSTRAINT FK_Parcel_Out_TO_File -- 분양 -> 파일
+    FOREIGN KEY (
+      PO_NUM -- 분양번호
+    )
+    REFERENCES Parcel_Out ( -- 분양
+      PO_NUM -- 분양번호
+    );
+
+-- 파일
+ALTER TABLE File
+  ADD CONSTRAINT FK_Mating_TO_File -- 짝짓기 -> 파일
+    FOREIGN KEY (
+      MAT_NUM -- 짝짓기번호
+    )
+    REFERENCES Mating ( -- 짝짓기
+      MAT_NUM -- 짝짓기번호
+    );
+
+-- 파일
+ALTER TABLE File
+  ADD CONSTRAINT FK_Education_TO_File -- 교육 -> 파일
+    FOREIGN KEY (
+      EDU_NUM -- 교육번호
+    )
+    REFERENCES Education ( -- 교육
+      EDU_NUM -- 교육번호
+    );
+
+-- 파일
+ALTER TABLE File
+  ADD CONSTRAINT FK_Schedule_TO_File -- 다이어리 - 일정 -> 파일
+    FOREIGN KEY (
+      SCH_NUM -- 일정번호
+    )
+    REFERENCES Schedule ( -- 다이어리 - 일정
+      SCH_NUM -- 일정번호
+    );
---
+++ animal/sql/data2.sql
@@ -0,0 +1,168 @@
+/* 회원 정보 입력 */
+insert into Member(MEM_NAME,MEM_EMAIL,MEM_PWD) 
+  values('관리자1','admin1@test.com','rpdlek');
+insert into Member(MEM_NAME,MEM_EMAIL,MEM_PWD) 
+  values('관리자2','admin2@test.com','rpdlek');
+insert into Member(MEM_NAME,MEM_EMAIL,MEM_PWD) 
+  values('관리자3','admin3@test.com','rpdlek');
+insert into Member(MEM_NAME,MEM_EMAIL,MEM_PWD) 
+  values('관리자4','admin4@test.com','rpdlek');
+
+/* 쪽지 내용 입력 */
+insert into Message(MEM_NUM,MEM_NUM2,MSG_CONTENT) values(1,2,'HIHI');
+insert into Message(MEM_NUM,MEM_NUM2,MSG_CONTENT) values(2,1,'HELLO');
+insert into Message(MEM_NUM,MEM_NUM2,MSG_CONTENT) values(3,2,'GAY');
+insert into Message(MEM_NUM,MEM_NUM2,MSG_CONTENT) values(2,4,'YO');
+insert into Message(MEM_NUM,MEM_NUM2,MSG_CONTENT) values(4,1,'CSS');
+
+/* 교육 내용 입력 */
+insert into Education(EDU_TITLE,EDU_CONTENT,EDU_DIV,EDU_CRE)
+  values('포메라이안 교육','교육이다아아앙1','개','2016-01-01');
+insert into Education(EDU_TITLE,EDU_CONTENT,EDU_DIV,EDU_CRE)
+  values('냐옹이 교육','교육이다아아앙2','고양이','2016-01-01');
+insert into Education(EDU_TITLE,EDU_CONTENT,EDU_DIV,EDU_CRE)
+  values('송사리 교육','교육이다아아앙3','물고기','2016-01-01');
+insert into Education(EDU_TITLE,EDU_CONTENT,EDU_DIV,EDU_CRE)
+  values('상어 교육','교육이다아아앙4','물고기','2016-01-01');
+insert into Education(EDU_TITLE,EDU_CONTENT,EDU_DIV,EDU_CRE)
+  values('앵무새 교육','교육이다아아앙5','새','2016-01-01');
+insert into Education(EDU_TITLE,EDU_CONTENT,EDU_DIV,EDU_CRE)
+  values('악어 교육','교육이다아아앙6','ETC','2016-01-01');
+insert into Education(EDU_TITLE,EDU_CONTENT,EDU_DIV,EDU_CRE)
+  values('타조 교육','교육이다아아앙7','새','2016-01-01');_CONTENT,EDU_DIV,EDU_CRE)
+  values('시베리안 허스키 교육','교육이다아아앙8','개','2016-01-01');
+insert into Education(EDU_TITLE,EDU_CONTENT,EDU_DIV,EDU_CRE)
+  values('페르시안 교육','교육이다아아앙9','고양이','2016-01-01');
+insert into Education(EDU_TITLE,EDU_CONTENT,EDU_DIV,EDU_CRE)
+  values('터키 앙고라 교육','교육이다아아앙10','고양이','2016-01-01');
+insert into Education(EDU_TITLE,EDU
+
+/* 내 펫 추가하기 */
+insert into Pet(MEM_NUM,PET_NAME,PET_SPECIES,TAG_COLOR)
+  values('1','강현찡','포메라이안','#FF1122');
+insert into Pet(MEM_NUM,PET_NAME,PET_SPECIES,TAG_COLOR)
+  values('1','상훈찡','시베리안허스키','#FF0000');
+insert into Pet(MEM_NUM,PET_NAME,PET_SPECIES,TAG_COLOR)
+  values('2','지호찡','자라','#FF1234');
+insert into Pet(MEM_NUM,PET_NAME,PET_SPECIES,TAG_COLOR)
+  values('3','대진꽁','거북이','#FF1122');
+insert into Pet(MEM_NUM,PET_NAME,PET_SPECIES,TAG_COLOR)
+  values('3','냥냥이','크로크다일','#FF0000');
+insert into Pet(MEM_NUM,PET_NAME,PET_SPECIES,TAG_COLOR)
+  values('4','갓쥐','이구아나','#FF1211');
+insert into Pet(MEM_NUM,PET_NAME,PET_SPECIES,TAG_COLOR)
+  values('4','야꽐라','코알라','#123421');
+
+  
+/* 짝짓기 내용 추가 */
+insert into Mating(MAT_TITLE,MAT_CONTENT,MAT_DIV,MAT_SPECIES,MAT_SEX,MAT_CRE)
+  values('우리 아이 짝짓기 부탁해요~~~1','서울 강남역 근처로 부탁드려요~','개','포메라이안','여자','2016-01-01');
+insert into Mating(MAT_TITLE,MAT_CONTENT,MAT_DIV,MAT_SPECIES,MAT_SEX,MAT_CRE)
+  values('우리 아이 짝짓기 부탁해요~~~2','서울 강남역 근처로 부탁드려요~','고양이','페르시안','남자','2016-01-01');
+insert into Mating(MAT_TITLE,MAT_CONTENT,MAT_DIV,MAT_SPECIES,MAT_SEX,MAT_CRE)
+  values('우리 아이 짝짓기 부탁해요~~~3','서울 강남역 근처로 부탁드려요~','개','시베리안허스키','여자','2016-01-01');
+insert into Mating(MAT_TITLE,MAT_CONTENT,MAT_DIV,MAT_SPECIES,MAT_SEX,MAT_CRE)
+  values('우리 아이 짝짓기 부탁해요~~~4','서울 강남역 근처로 부탁드려요~','개','슈나우져','여자','2016-01-01');
+insert into Mating(MAT_TITLE,MAT_CONTENT,MAT_DIV,MAT_SPECIES,MAT_SEX,MAT_CRE)
+  values('우리 아이 짝짓기 부탁해요~~~5','서울 강남역 근처로 부탁드려요~','개','불독','여자','2016-01-01');
+insert into Mating(MAT_TITLE,MAT_CONTENT,MAT_DIV,MAT_SPECIES,MAT_SEX,MAT_CRE)
+  values('우리 아이 짝짓기 부탁해요~~~6','서울 강남역 근처로 부탁드려요~','고양이','집고양이','남자','2016-01-01');
+insert into Mating(MAT_TITLE,MAT_CONTENT,MAT_DIV,MAT_SPECIES,MAT_SEX,MAT_CRE)
+  values('우리 아이 짝짓기 부탁해요~~~7','우리 아이 부탁드려요~~~~','ETC','악어','여자','2016-01-01');
+insert into Mating(MAT_TITLE,MAT_CONTENT,MAT_DIV,MAT_SPECIES,MAT_SEX,MAT_CRE)
+  values('우리 아이 짝짓기 부탁해요~~~8','나이는 어려용~~~','새','비둘기','남자','2016-01-01');
+insert into Mating(MAT_TITLE,MAT_CONTENT,MAT_DIV,MAT_SPECIES,MAT_SEX,MAT_CRE)
+  values('우리 아이 짝짓기 부탁해요~~~9','서울 강남역 근처로 부탁드려요~','새','앵무새','남자','2016-01-01');
+insert into Mating(MAT_TITLE,MAT_CONTENT,MAT_DIV,MAT_SPECIES,MAT_SEX,MAT_CRE)
+  values('우리 아이 짝짓기 부탁해요~~~10','서울 강남역 근처로 부탁드려요~','개','누렁이','남자','2016-01-01');
+insert into Mating(MAT_TITLE,MAT_CONTENT,MAT_DIV,MAT_SPECIES,MAT_SEX,MAT_CRE)
+  values('우리 아이 한번 하게 해주~~~11','서울 강남역 근처로 부탁드려요~','개','진돗개','남자','2016-01-01');
+
+/* 분양 글쓰기 */
+insert into Parcel_Out(PO_TITLE,PO_CONTENT,PO_DIV,PO_SPECIES,PO_CREDT)
+  values('퓨마 분양해드려요~~','급한 사정이 있어 빠른 분양 부탁드려요ㅠㅠ','ETC','퓨마','2016-01-01');
+insert into Parcel_Out(PO_TITLE,PO_CONTENT,PO_DIV,PO_SPECIES,PO_CREDT)
+  values('콩이 분양해드려요~~','급한 사정이 있어 빠른 분양 부탁드려요ㅠㅠ','개','시츄','2016-01-01');
+insert into Parcel_Out(PO_TITLE,PO_CONTENT,PO_DIV,PO_SPECIES,PO_CREDT)
+  values('츄롤리 분양해드려요~~','급한 사정이 있어 빠른 분양 부탁드려요ㅠㅠ','ETC','거북이','2016-01-01');
+insert into Parcel_Out(PO_TITLE,PO_CONTENT,PO_DIV,PO_SPECIES,PO_CREDT)
+  values('꽐라 분양해드려요~~','급한 사정이 있어 빠른 분양 부탁드려요ㅠㅠ','ETC','코알라','2016-01-01');
+insert into Parcel_Out(PO_TITLE,PO_CONTENT,PO_DIV,PO_SPECIES,PO_CREDT)
+  values('꽁양이 분양해드려요~~','급한 사정이 있어 빠른 분양 부탁드려요ㅠㅠ','새','앵무새','2016-01-01');
+insert into Parcel_Out(PO_TITLE,PO_CONTENT,PO_DIV,PO_SPECIES,MEM_NUM,PO_CREDT)
+  values('퓨마 분양해드려요~~','급한 사정이 있어 빠른 분양 부탁드려요ㅠㅠ','ETC','퓨마',1,'2016-01-01');
+insert into Parcel_Out(PO_TITLE,PO_CONTENT,PO_DIV,PO_SPECIES,MEM_NUM,PO_CREDT)
+  values('콩이 분양해드려요~~','급한 사정이 있어 빠른 분양 부탁드려요ㅠㅠ','개','시츄',2,'2016-01-01');
+insert into Parcel_Out(PO_TITLE,PO_CONTENT,PO_DIV,PO_SPECIES,MEM_NUM,PO_CREDT)
+  values('츄롤리 분양해드려요~~','급한 사정이 있어 빠른 분양 부탁드려요ㅠㅠ','ETC','거북이',2,'2016-01-01');
+insert into Parcel_Out(PO_TITLE,PO_CONTENT,PO_DIV,PO_SPECIES,MEM_NUM,PO_CREDT)
+  values('꽐라 분양해드려요~~','급한 사정이 있어 빠른 분양 부탁드려요ㅠㅠ','ETC','코알라',3,'2016-01-01');
+insert into Parcel_Out(PO_TITLE,PO_CONTENT,PO_DIV,PO_SPECIES,MEM_NUM,PO_CREDT)
+  values('꽁양이 분양해드려요~~','급한 사정이 있어 빠른 분양 부탁드려요ㅠㅠ','새','앵무새',4,'2016-01-01');
+  
+  
+/* Comment */
+insert into Comment(CMT_CONTENT,MEM_NUM,SCH_NUM) values('너무 이뻐요~',1,2);
+insert into Comment(CMT_CONTENT,MEM_NUM,SCH_NUM) values('너무 이뻐요~',3,2);
+insert into Comment(CMT_CONTENT,MEM_NUM,SCH_NUM) values('너무 이뻐요~',4,2);
+insert into Comment(CMT_CONTENT,MEM_NUM,EDU_NUM) 
+  values('잘 키우고 싶네여 도움이 많이 되요~',1,1);
+insert into Comment(CMT_CONTENT,MEM_NUM,EDU_NUM) values('너무 이뻐요~',2,2);
+insert into Comment(CMT_CONTENT,MEM_NUM,EDU_NUM) 
+  values('이런 방법이 있군요~',3,3);
+insert into Comment(CMT_CONTENT,MEM_NUM,EDU_NUM) values('너무 이뻐요~',1,1);
+insert into Comment(CMT_CONTENT,MEM_NUM,EDU_NUM) values('너무 이뻐요~',1,1);
+insert into Comment(CMT_CONTENT,MEM_NUM,EDU_NUM) values('너무 이뻐요~',1,3);
+insert into Comment(CMT_CONTENT,MEM_NUM,PO_NUM) 
+  values('너무 분양 받고 싶어요~~',1,2);
+insert into Comment(CMT_CONTENT,MEM_NUM,PO_NUM) 
+  values('죄송해요 분양이 끝났어요~',2,2);
+insert into Comment(CMT_CONTENT,MEM_NUM,PO_NUM) 
+  values('너무 아쉽네~',1,2);
+insert into Comment(CMT_CONTENT,MEM_NUM,PO_NUM) 
+  values('이쁜 아이 데려가고 싶네요~',3,1);
+insert into Comment(CMT_CONTENT,MEM_NUM,MAT_NUM) 
+  values('저희 아가랑 짝짓기 하실래요?',1,2);
+insert into Comment(CMT_CONTENT,MEM_NUM,MAT_NUM) 
+  values('네 ~ 좋아요~',1,2);
+insert into Comment(CMT_CONTENT,MEM_NUM,MAT_NUM) 
+  values('꽐라꽐라~~~~',3,9);
+  
+  
+/* 카테고리 명 등록 */
+insert into Category(CATE_CODE,CATE_NAME) values('tab001','자랑하기');
+insert into Category(CATE_CODE,CATE_NAME) values('tab002','산책코스');
+
+/* 일정등록 */
+insert into Schedule(PET_NUM,START_DATE,END_DATE,SCH_CRE,NOTI_OPEN)
+  values(6,'2016-01-01','2016-01-10','2016-01-10',0);
+insert into Schedule(PET_NUM,START_DATE,END_DATE,SCH_CRE,NOTI_OPEN)
+  values(5,'2016-01-01','2016-01-10','2016-01-10',0);
+insert into Schedule(PET_NUM,START_DATE,END_DATE,SCH_CRE,NOTI_OPEN)
+  values(2,'2016-01-01','2016-01-10','2016-01-10',0);
+insert into Schedule(PET_NUM,START_DATE,END_DATE,SCH_CRE,NOTI_OPEN)
+  values(3,'2016-01-01','2016-01-10','2016-01-10',0);
+  
+/* 자랑하기 등록 */
+insert into Schedule(PET_NUM,START_DATE,END_DATE,SCH_TITLE,SCH_CONTENT,CATE_CODE,NOTI_OPEN,SCH_CRE)
+  values(4,now(),now(),'너무 이쁘죠~~?','제 말을 너무 잘 들어요~','tab001',0,'2016-01-10');
+insert into Schedule(PET_NUM,START_DATE,END_DATE,SCH_TITLE,SCH_CONTENT,CATE_CODE,NOTI_OPEN,SCH_CRE)
+  values(2,now(),now(),'반해버렸어요','너무 이쁜거 있죠?','tab001',1,'2016-01-10');
+insert into Schedule(PET_NUM,START_DATE,END_DATE,SCH_TITLE,SCH_CONTENT,CATE_CODE,NOTI_OPEN,SCH_CRE)
+  values(1,now(),now(),'너무 이쁘죠~~?','얄라리 얄라 얄라리얄라','tab001',1,'2016-01-10');
+insert into Schedule(PET_NUM,START_DATE,END_DATE,SCH_TITLE,SCH_CONTENT,CATE_CODE,NOTI_OPEN,SCH_CRE)
+  values(1,now(),now(),'심쿵해~~~~~','제 말을 너무 잘 들어요~','tab001',0,'2016-01-10');
+insert into Schedule(PET_NUM,START_DATE,END_DATE,SCH_TITLE,SCH_CONTENT,CATE_CODE,NOTI_OPEN,SCH_CRE)
+  values(3,now(),now(),'너무 이쁘죠~~?','제 말을 너무 잘 들어요~','tab001',1,'2016-01-10');
+
+/* 산책코스 등록 */
+insert into Schedule(PET_NUM,START_DATE,END_DATE,SCH_TITLE,SCH_CONTENT,CATE_CODE,NOTI_OPEN,SCH_CRE)
+  values(5,now(),now(),'콩이랑 다녀왔어요~','너무 이쁜거 있죠? 여러분도 다녀오세요','tab002',0,'2016-01-10');
+insert into Schedule(PET_NUM,START_DATE,END_DATE,SCH_TITLE,SCH_CONTENT,CATE_CODE,NOTI_OPEN,SCH_CRE)
+  values(3,now(),now(),'반해버렸어요','너무 이쁜거 있죠?','tab002',1,'2016-01-10');
+insert into Schedule(PET_NUM,START_DATE,END_DATE,SCH_TITLE,SCH_CONTENT,CATE_CODE,NOTI_OPEN,SCH_CRE)
+  values(3,now(),now(),'너무 이쁘죠~~?','얄라리 얄라 얄라리얄라','tab002',1,'2016-01-10');
+insert into Schedule(PET_NUM,START_DATE,END_DATE,SCH_TITLE,SCH_CONTENT,CATE_CODE,NOTI_OPEN,SCH_CRE)
+  values(7,now(),now(),'심쿵해~~~~~','제 말을 너무 잘 들어요~','tab002',0,'2016-01-10');
+insert into Schedule(PET_NUM,START_DATE,END_DATE,SCH_TITLE,SCH_CONTENT,CATE_CODE,NOTI_OPEN,SCH_CRE)
+  values(6,now(),now(),'너무 이쁘죠~~?','제 말을 너무 잘 들어요~','tab002',1,'2016-01-10');
