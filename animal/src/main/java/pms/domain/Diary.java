d68c1d8fb410ca7e789676e2d288fa640812a643
KimDaeJin <daejinious@gmail.com>
2016-01-07 15:23:36 +0900

    data.sql 최종 / Diary 주석 수정(일정 코드 부분)

--- animal/src/main/java/pms/domain/Diary.java
+++ animal/src/main/java/pms/domain/Diary.java
@@ -13,7 +13,7 @@ public class Diary  implements Serializable {
   protected Date createDate;	// 작성일
   protected String content;	// 내용
   protected String title;		// 제목
-  protected String cateCode;	//카테고리 코드	 tab001=자랑하기 tab002=산책코스
+  protected String cateCode;	//카테고리 코드 ILJUNG=일정 JARANG=자랑하기 SANCHE=산책코스
   protected boolean dhide;		// 공개여부	0=공개 1=비공개
   protected int view;				// 조회수    default=0
   protected int like;				// 좋아요수  default=0
