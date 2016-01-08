236c26f2ce501801110154dfe7e54a8d99b8ff05
lsanghoon <isanghoon@isanghooni-MacBook-Pro.local>
2016-01-08 09:58:27 +0900

    오호라

--- animal/src/main/java/pms/controller/ajax/DiaryController.java
+++ animal/src/main/java/pms/controller/ajax/DiaryController.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
 
 import pms.domain.AjaxResult;
 import pms.domain.Diary;
@@ -40,28 +39,28 @@ public class DiaryController {
 		List<Diary> events = diaryService.getEventList();
 
 		HashMap<String,Object> resultMap = new HashMap<>();
-		ModelAndView mav = new ModelAndView();
+//		ModelAndView mav = new ModelAndView();
 		resultMap.put("status", "success");
 		resultMap.put("pets", pets);
-		try {
-			if(events!=null) {
-				for (int i=0; i<=events.size(); i++) {
-					resultMap.put("id", events.get(i).getDno());
-					resultMap.put("start", events.get(i).getStartDate());
-					resultMap.put("end", events.get(i).getEndDate());
-					resultMap.put("title", events.get(i).getTitle());
-					resultMap.put("content", events.get(i).getContent());
-					resultMap.put("hide", events.get(i).isDhide());
-					resultMap.put("color", events.get(i).getTagColor());
-					
-				}
-				mav.addObject("data", resultMap);
-				mav.setViewName("jsonView");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-//		resultMap.put("events", events);
+//		try {
+//			if(events!=null) {
+//				for (int i=0; i<=events.size(); i++) {
+//					resultMap.put("id", events.get(i).getDno());
+//					resultMap.put("start", events.get(i).getStartDate());
+//					resultMap.put("end", events.get(i).getEndDate());
+//					resultMap.put("title", events.get(i).getTitle());
+//					resultMap.put("content", events.get(i).getContent());
+//					resultMap.put("hide", events.get(i).isDhide());
+//					resultMap.put("color", events.get(i).getTagColor());
+//					
+//				}
+//				mav.addObject("data", resultMap);
+//				mav.setViewName("jsonView");
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+		resultMap.put("events", events);
 		
 		return resultMap;
 		//return mav;
