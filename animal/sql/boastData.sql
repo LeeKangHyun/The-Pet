3952b2190995d1e9f38a4e3830be536a774607db
jeongjiho <jeongjiho@jeongjihoui-MacBook-Pro.local>
2016-01-18 09:26:52 +0900

    지호-지호지호지호

--- animal/sql/boastData.sql
+++ animal/sql/boastData.sql
@@ -18,3 +18,130 @@ insert into Files(FILE_NAME,SCH_NUM) values('h4.jpg','22');
 insert into Files(FILE_NAME,SCH_NUM) values('h5.jpeg','33');
 insert into Files(FILE_NAME,SCH_NUM) values('h6.png','36');
 
+insert into Files(FILE_NAME,SCH_NUM) values('h1.jpg','1');
+insert into Files(FILE_NAME,SCH_NUM) values('h2.jpg','2');
+insert into Files(FILE_NAME,SCH_NUM) values('h3.jpg','3');
+insert into Files(FILE_NAME,SCH_NUM) values('h4.jpg','4');
+insert into Files(FILE_NAME,SCH_NUM) values('h5.jpeg','5');
+insert into Files(FILE_NAME,SCH_NUM) values('h6.png','6');
+insert into Files(FILE_NAME,SCH_NUM) values('h7.jpg','7');
+insert into Files(FILE_NAME,SCH_NUM) values('h8.png','8');
+insert into Files(FILE_NAME,SCH_NUM) values('h9.jpg','9');
+insert into Files(FILE_NAME,SCH_NUM) values('h10.gif','10');
+insert into Files(FILE_NAME,SCH_NUM) values('h1.jpg','11');
+insert into Files(FILE_NAME,SCH_NUM) values('h2.jpg','12');
+insert into Files(FILE_NAME,SCH_NUM) values('h1.jpg','13');
+insert into Files(FILE_NAME,SCH_NUM) values('h2.jpg','14');
+insert into Files(FILE_NAME,SCH_NUM) values('h3.jpg','15');
+insert into Files(FILE_NAME,SCH_NUM) values('h4.jpg','16');
+insert into Files(FILE_NAME,SCH_NUM) values('h5.jpeg','17');
+insert into Files(FILE_NAME,SCH_NUM) values('h6.png','18');
+insert into Files(FILE_NAME,SCH_NUM) values('h7.jpg','19');
+insert into Files(FILE_NAME,SCH_NUM) values('h8.png','20');
+insert into Files(FILE_NAME,SCH_NUM) values('h9.jpg','21');
+insert into Files(FILE_NAME,SCH_NUM) values('h10.gif','22');
+insert into Files(FILE_NAME,SCH_NUM) values('h1.jpg','23');
+insert into Files(FILE_NAME,SCH_NUM) values('h2.jpg','24');
+insert into Files(FILE_NAME,SCH_NUM) values('h3.jpg','25');
+insert into Files(FILE_NAME,SCH_NUM) values('h4.jpg','26');
+insert into Files(FILE_NAME,SCH_NUM) values('h5.jpeg','27');
+insert into Files(FILE_NAME,SCH_NUM) values('h6.png','28');
+insert into Files(FILE_NAME,SCH_NUM) values('h7.jpg','29');
+insert into Files(FILE_NAME,SCH_NUM) values('h8.png','30');
+insert into Files(FILE_NAME,SCH_NUM) values('h9.jpg','31');
+insert into Files(FILE_NAME,SCH_NUM) values('h10.gif','32');
+insert into Files(FILE_NAME,SCH_NUM) values('h1.jpg','33');
+insert into Files(FILE_NAME,SCH_NUM) values('h2.jpg','34');
+insert into Files(FILE_NAME,SCH_NUM) values('h3.jpg','35');
+insert into Files(FILE_NAME,SCH_NUM) values('h4.jpg','36');
+insert into Files(FILE_NAME,SCH_NUM) values('h5.jpeg','37');
+insert into Files(FILE_NAME,SCH_NUM) values('h6.png','38');
+insert into Files(FILE_NAME,SCH_NUM) values('h7.jpg','39');
+insert into Files(FILE_NAME,SCH_NUM) values('h8.png','40');
+insert into Files(FILE_NAME,SCH_NUM) values('h9.jpg','41');
+insert into Files(FILE_NAME,SCH_NUM) values('h10.gif','42');
+insert into Files(FILE_NAME,SCH_NUM) values('h1.jpg','43');
+insert into Files(FILE_NAME,SCH_NUM) values('h2.jpg','44');
+insert into Files(FILE_NAME,SCH_NUM) values('h3.jpg','45');
+insert into Files(FILE_NAME,SCH_NUM) values('h4.jpg','46');
+insert into Files(FILE_NAME,SCH_NUM) values('h5.jpeg','47');
+insert into Files(FILE_NAME,SCH_NUM) values('h6.png','48');
+insert into Files(FILE_NAME,SCH_NUM) values('h7.jpg','49');
+insert into Files(FILE_NAME,SCH_NUM) values('h8.png','50');
+insert into Files(FILE_NAME,SCH_NUM) values('h9.jpg','51');
+insert into Files(FILE_NAME,SCH_NUM) values('h10.gif','52');
+insert into Files(FILE_NAME,SCH_NUM) values('h1.jpg','53');
+insert into Files(FILE_NAME,SCH_NUM) values('h2.jpg','54');
+insert into Files(FILE_NAME,SCH_NUM) values('h3.jpg','55');
+insert into Files(FILE_NAME,SCH_NUM) values('h4.jpg','56');
+insert into Files(FILE_NAME,SCH_NUM) values('h5.jpeg','57');
+insert into Files(FILE_NAME,SCH_NUM) values('h6.png','58');
+insert into Files(FILE_NAME,SCH_NUM) values('h7.jpg','59');
+insert into Files(FILE_NAME,SCH_NUM) values('h8.png','60');
+insert into Files(FILE_NAME,SCH_NUM) values('h9.jpg','61');
+insert into Files(FILE_NAME,SCH_NUM) values('h10.gif','62');
+insert into Files(FILE_NAME,SCH_NUM) values('h1.jpg','63');
+insert into Files(FILE_NAME,SCH_NUM) values('h2.jpg','64');
+insert into Files(FILE_NAME,SCH_NUM) values('h3.jpg','65');
+insert into Files(FILE_NAME,SCH_NUM) values('h4.jpg','66');
+insert into Files(FILE_NAME,SCH_NUM) values('h5.jpeg','67');
+insert into Files(FILE_NAME,SCH_NUM) values('h6.png','68');
+insert into Files(FILE_NAME,SCH_NUM) values('h7.jpg','69');
+insert into Files(FILE_NAME,SCH_NUM) values('h8.png','70');
+insert into Files(FILE_NAME,SCH_NUM) values('h9.jpg','71');
+insert into Files(FILE_NAME,SCH_NUM) values('h10.gif','72');
+insert into Files(FILE_NAME,SCH_NUM) values('h1.jpg','73');
+insert into Files(FILE_NAME,SCH_NUM) values('h2.jpg','74');
+insert into Files(FILE_NAME,SCH_NUM) values('h3.jpg','75');
+insert into Files(FILE_NAME,SCH_NUM) values('h4.jpg','76');
+insert into Files(FILE_NAME,SCH_NUM) values('h5.jpeg','77');
+insert into Files(FILE_NAME,SCH_NUM) values('h6.png','78');
+insert into Files(FILE_NAME,SCH_NUM) values('h7.jpg','79');
+insert into Files(FILE_NAME,SCH_NUM) values('h8.png','80');
+insert into Files(FILE_NAME,SCH_NUM) values('h9.jpg','81');
+insert into Files(FILE_NAME,SCH_NUM) values('h10.gif','82');
+insert into Files(FILE_NAME,SCH_NUM) values('h1.jpg','83');
+insert into Files(FILE_NAME,SCH_NUM) values('h2.jpg','84');
+insert into Files(FILE_NAME,SCH_NUM) values('h3.jpg','85');
+insert into Files(FILE_NAME,SCH_NUM) values('h4.jpg','86');
+insert into Files(FILE_NAME,SCH_NUM) values('h5.jpeg','87');
+insert into Files(FILE_NAME,SCH_NUM) values('h6.png','88');
+insert into Files(FILE_NAME,SCH_NUM) values('h7.jpg','89');
+insert into Files(FILE_NAME,SCH_NUM) values('h8.png','90');
+insert into Files(FILE_NAME,SCH_NUM) values('h9.jpg','91');
+
+
+
+
+/* 자랑하기 등록 */
+insert into 
+  Schedule(PET_NUM,MEM_NUM,START_DATE,END_DATE,SCH_CRE,SCH_TITLE,SCH_CONTENT,CATE_CODE,NOTI_HIDE,TAG_COLOR,PET_SPEC,NOTI_HIDE,NOTI_VIEWS)
+  values(5,4,'2016-01-25','2016-02-25',now(),'너무 이쁘죠~~?','제 말을 너무 잘 들어요~','JARANG',true,'#123098','DOG',10,20);
+insert into 
+  Schedule(PET_NUM,MEM_NUM,START_DATE,END_DATE,SCH_CRE,SCH_TITLE,SCH_CONTENT,CATE_CODE,NOTI_HIDE,TAG_COLOR,PET_SPEC,NOTI_HIDE,NOTI_VIEWS)
+  values(6,5,'2016-01-25','2016-01-25',now(),'반해버렸어요','너무 이쁜거 있죠?','JARANG',true,'#123456','CAT',15,25);
+insert into 
+  Schedule(PET_NUM,MEM_NUM,START_DATE,END_DATE,SCH_CRE,SCH_TITLE,SCH_CONTENT,CATE_CODE,NOTI_HIDE,TAG_COLOR,PET_SPEC,NOTI_HIDE,NOTI_VIEWS)
+  values(7,6,'2016-01-20','2016-01-20',now(),'너무 이쁘죠~~?','얄라리 얄라 얄라리얄라','JARANG',true,'#000000','FISH',9,11);
+insert into 
+  Schedule(PET_NUM,MEM_NUM,START_DATE,END_DATE,SCH_CRE,SCH_TITLE,SCH_CONTENT,CATE_CODE,NOTI_HIDE,TAG_COLOR,PET_SPEC,NOTI_HIDE,NOTI_VIEWS)
+  values(8,7,'2016-01-28','2016-01-28',now(),'심쿵해~~~~~','제 말을 너무 잘 들어요~','JARANG',true,'#000000','BIRD',2,5);
+insert into 
+  Schedule(PET_NUM,MEM_NUM,START_DATE,END_DATE,SCH_CRE,SCH_TITLE,SCH_CONTENT,CATE_CODE,NOTI_HIDE,TAG_COLOR,PET_SPEC,NOTI_HIDE,NOTI_VIEWS)
+  values(9,8,'2016-01-29','2016-01-29',now(),'너무 이쁘죠~~?','제 말을 너무 잘 들어요~','JARANG',true,'#001122','ETC',10,2);
+
+insert into 
+  Schedule(PET_NUM,MEM_NUM,START_DATE,END_DATE,SCH_CRE,SCH_TITLE,SCH_CONTENT,CATE_CODE,NOTI_HIDE,TAG_COLOR,PET_SPEC,NOTI_HIDE,NOTI_VIEWS)
+  values(10,9,'2016-01-25','2016-02-25',now(),'너무 이쁘죠~~?','제 말을 너무 잘 들어요~','JARANG',true,'#123098','DOG',104,230);
+insert into 
+  Schedule(PET_NUM,MEM_NUM,START_DATE,END_DATE,SCH_CRE,SCH_TITLE,SCH_CONTENT,CATE_CODE,NOTI_HIDE,TAG_COLOR,PET_SPEC,NOTI_HIDE,NOTI_VIEWS)
+  values(11,10,'2016-01-25','2016-01-25',now(),'반해버렸어요','너무 이쁜거 있죠?','JARANG',1,'#123456','CAT',120,202);
+insert into 
+  Schedule(PET_NUM,MEM_NUM,START_DATE,END_DATE,SCH_CRE,SCH_TITLE,SCH_CONTENT,CATE_CODE,NOTI_HIDE,TAG_COLOR,PET_SPEC,NOTI_HIDE,NOTI_VIEWS)
+  values(12,11,'2016-01-20','2016-01-20',now(),'너무 이쁘죠~~?','얄라리 얄라 얄라리얄라','JARANG',1,'#000000','FISH',12,59);
+insert into 
+  Schedule(PET_NUM,MEM_NUM,START_DATE,END_DATE,SCH_CRE,SCH_TITLE,SCH_CONTENT,CATE_CODE,NOTI_HIDE,TAG_COLOR,PET_SPEC,NOTI_HIDE,NOTI_VIEWS)
+  values(13,12,'2016-01-28','2016-01-28',now(),'심쿵해~~~~~','제 말을 너무 잘 들어요~','JARANG',1,'#000000','BIRD',60,44);
+insert into 
+  Schedule(PET_NUM,MEM_NUM,START_DATE,END_DATE,SCH_CRE,SCH_TITLE,SCH_CONTENT,CATE_CODE,NOTI_HIDE,TAG_COLOR,PET_SPEC,NOTI_HIDE,NOTI_VIEWS)
+  values(14,13,'2016-01-29','2016-01-29',now(),'너무 이쁘죠~~?','제 말을 너무 잘 들어요~','JARANG',1,'#001122','ETC',20,40);
