27e1bce5cd204ee90766335ddd3dda3dd1485da9
Kanghyun Lee <rkdgusdl92@gmail.com>
2016-02-03 11:46:31 +0900

    강현. detail 페이지

--- animal/src/main/java/pms/domain/Walk.java
+++ animal/src/main/java/pms/domain/Walk.java
@@ -9,38 +9,58 @@ public class Walk implements Serializable {
   protected String locLat;
   protected String locLng;
   protected String locName;
+  protected String locDong;  
   protected int dno;
   
   public Walk () {}
 
-	public int getLno() {
-		return lno;
-	}
-	public void setLno(int lno) {
-		this.lno = lno;
-	}
-	public String getLocLat() {
-		return locLat;
-	}
-	public void setLocLat(String locLat) {
-		this.locLat = locLat;
-	}
-	public String getLocLng() {
-		return locLng;
-	}
-	public void setLocLng(String locLng) {
-		this.locLng = locLng;
-	}
-	public String getLocName() {
-		return locName;
-	}
-	public void setLocName(String locName) {
-		this.locName = locName;
-	}
-	public int getDno() {
-		return dno;
-	}
-	public void setDno(int dno) {
-		this.dno = dno;
-	}
+  public int getLno() {
+    return lno;
+  }
+
+  public void setLno(int lno) {
+    this.lno = lno;
+  }
+
+  public String getLocLat() {
+    return locLat;
+  }
+
+  public void setLocLat(String locLat) {
+    this.locLat = locLat;
+  }
+
+  public String getLocLng() {
+    return locLng;
+  }
+
+  public void setLocLng(String locLng) {
+    this.locLng = locLng;
+  }
+
+  public String getLocName() {
+    return locName;
+  }
+
+  public void setLocName(String locName) {
+    this.locName = locName;
+  }
+
+  public String getLocDong() {
+    return locDong;
+  }
+
+  public void setLocDong(String locDong) {
+    this.locDong = locDong;
+  }
+
+  public int getDno() {
+    return dno;
+  }
+
+  public void setDno(int dno) {
+    this.dno = dno;
+  }
+
+	
 }
