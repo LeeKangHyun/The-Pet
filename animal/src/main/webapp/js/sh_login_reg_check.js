9be0ec8b07aef40ca2e1e9e4bcea86903de78ad5
lsanghoon <isanghoon@SHs-MacBook-Pro.local>
2016-02-12 16:45:24 +0900

    no message

--- animal/src/main/webapp/js/sh_login_reg_check.js
+++ animal/src/main/webapp/js/sh_login_reg_check.js
@@ -28,7 +28,8 @@ function check(id) {
     }
     
     if (id == "password" || id == "password2") {
-        var isPassword = /^(?=.*[a-zA-Z])((?=.*[0-9])(?=.*[!@#$%^&*()_-+.?])).{8,20}$/;
+//        var isPassword = /^(?=.*[a-zA-Z])((?=.*[0-9])(?=.*[!@#$%^&*()_-+.?])).{8,20}$/;
+        var isPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
         if (!isPassword.test(oid)) {
             oDiv.removeClass("has-success");
             oDiv.addClass("has-error");
