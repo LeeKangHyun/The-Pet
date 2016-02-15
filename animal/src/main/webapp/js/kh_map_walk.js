c1c2003a0e47d370106ca944c62cfc46522f8cf7
Kanghyun Lee <rkdgusdl92@gmail.com>
2016-02-15 20:04:33 +0900

    강현_ 사소한 정리

--- animal/src/main/webapp/js/kh_map_walk.js
+++ animal/src/main/webapp/js/kh_map_walk.js
@@ -1,14 +1,13 @@
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+var mapContainer = document.getElementById('map'),
 mapOption = {
-    center: new daum.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
-    level: 3 // 지도의 확대 레벨
+    center: new daum.maps.LatLng(37.566826, 126.9786567),
+    level: 3
 };
-// 지도를 생성합니다
 var map = new daum.maps.Map(mapContainer, mapOption);
 
 navigator.geolocation.getCurrentPosition(function (position) {
-  var lat = position.coords.latitude, // 위도
-      lon = position.coords.longitude; // 경도
+  var lat = position.coords.latitude,
+      lon = position.coords.longitude;
   var locPosition = new daum.maps.LatLng(lat, lon);
 
   map.setCenter(locPosition);
@@ -20,21 +19,15 @@ navigator.geolocation.getCurrentPosition(function (position) {
 
 var dotlng, dotlat;
 var circleOverlay;
-var aaa = [];
 var i = 0;
-// 지도에 클릭 이벤트를 등록합니다
-// 지도를 클릭하면 선 그리기가 시작됩니다 그려진 선이 있으면 지우고 다시 그립니다
 daum.maps.event.addListener(map, 'click', function(mouseEvent) {
     if (i === 0) {
         var zb = mouseEvent.latLng.zb;
         var Ab = mouseEvent.latLng.Ab;
-        console.log(mouseEvent);
-        // 마우스로 클릭한 위치입니다
         var clickPosition = mouseEvent.latLng;
         dotlng = zb;
         dotlat = Ab;
         displayCircleDot(clickPosition);
-
         i++;
     } else {
         alert('1곳만 선택할수 있습니다.');
@@ -43,30 +36,22 @@ daum.maps.event.addListener(map, 'click', function(mouseEvent) {
     }
 });
 
-
-// 지도에 마우스 오른쪽 클릭 이벤트를 등록합니다
-// 선을 그리고있는 상태에서 마우스 오른쪽 클릭 이벤트가 발생하면 선 그리기를 종료합니다
 daum.maps.event.addListener(map, 'rightclick', function(mouseEvent) {
     i = 0;
     deleteCircleDot();
 });
 
-// 선이 그려지고 있는 상태일 때 지도를 클릭하면 호출하여
-// 클릭 지점에 대한 정보 (동그라미)를 표출하는 함수입니다
 function displayCircleDot(position) {
 
-    // 클릭 지점을 표시할 빨간 동그라미 커스텀오버레이를 생성합니다
     circleOverlay = new daum.maps.CustomOverlay({
         content: '<span class="dot"></span>',
         position: position,
         zIndex: 1
     });
 
-    // 지도에 표시합니다
     circleOverlay.setMap(map);
 }
 
-// 클릭 지점에 대한 정보 (동그라미)를 지도에서 모두 제거하는 함수입니다
 function deleteCircleDot() {
     circleOverlay.setMap(null);
 }
